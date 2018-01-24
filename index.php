936c7ce0eb99e27c3a00720dd6d11a785ed42b61
Shaowei Zhang <zsw22@vip.qq.com>
2018-01-24 15:58:20 +1100

    insert post image and display it

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
                 <hr>
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span
