054021112c5097f15826c0901f330b04386b620c
Shaowei Zhang <zsw22@vip.qq.com>
2018-01-24 15:13:39 +1100

    insert data into the post table and display

--- index.php
+++ index.php
@@ -12,31 +12,39 @@
 
         <!-- Blog Entries Column -->
         <div class="col-md-8">
-
             <h1 class="page-header">
                 Page Heading
                 <small>Secondary Text</small>
             </h1>
 
-            <!-- First Blog Post -->
-            <h2>
-                <a href="#">Blog Post Title</a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php">Start Bootstrap</a>
-            </p>
-            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-            <hr>
-            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus
-                inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis
-                ipsum
-                officiis rerum.</p>
-            <a class="btn btn-primary" href="#">Read More <span
-                        class="glyphicon glyphicon-chevron-right"></span></a>
+			<?php
+			$query = 'SELECT * FROM posts';
+			$select_posts = $db->query($query);
+			while ($row = $select_posts->fetch()) :
+				$post_title = $row['post_title'];
+				$post_author = $row['post_author'];
+				$post_date = date('M d Y', strtotime($row['post_date']));
+				$post_image = $row['post_image'];
+				$post_content = $row['post_content'];
+				?>
 
-            <hr>
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <hr>
+                <p><?php echo $post_content ?></p>
+                <a class="btn btn-primary" href="#">Read More <span
+                            class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+			<?php endwhile; ?>
 
         </div>
 
