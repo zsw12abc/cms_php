340b8f1cd65e78d639a3670ca1c71522f0cd989e
Shaowei Zhang <zsw22@vip.qq.com>
2018-01-24 19:13:29 +1100

    createing resuable code in admin

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -34,18 +34,18 @@
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
-<!--            <div class="col-lg-6">-->
-<!--                <ul class="list-unstyled">-->
-<!--                    <li><a href="#">Category Name</a>-->
-<!--                    </li>-->
-<!--                    <li><a href="#">Category Name</a>-->
-<!--                    </li>-->
-<!--                    <li><a href="#">Category Name</a>-->
-<!--                    </li>-->
-<!--                    <li><a href="#">Category Name</a>-->
-<!--                    </li>-->
-<!--                </ul>-->
-<!--            </div>-->
+            <!--            <div class="col-lg-6">-->
+            <!--                <ul class="list-unstyled">-->
+            <!--                    <li><a href="#">Category Name</a>-->
+            <!--                    </li>-->
+            <!--                    <li><a href="#">Category Name</a>-->
+            <!--                    </li>-->
+            <!--                    <li><a href="#">Category Name</a>-->
+            <!--                    </li>-->
+            <!--                    <li><a href="#">Category Name</a>-->
+            <!--                    </li>-->
+            <!--                </ul>-->
+            <!--            </div>-->
             <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
