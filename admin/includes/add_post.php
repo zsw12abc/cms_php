07cd03c52fc08989111a76cbee2d57fc00c0a323
Shaowei Zhang <zsw22@vip.qq.com>
2018-01-25 17:23:14 +1100

    add addPosts by PHP

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -12,6 +12,13 @@ if (isset($_POST['create_post'])) {
 	$post_comment_count = 0;
 
 	move_uploaded_file($post_image_temp, "../images/$post_image");
+
+	$add_posts_query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status)"
+		. "VALUES('{$post_category_id}', '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}')";
+	$add_posts = $db->query($add_posts_query);
+	if (!$add_posts) {
+		die('Add Posts Failed');
+	}
 }
 ?>
 
